611dd2e4437c95588a06751d2f1b865bd386d123
atsu41 <jijvha.4114ref@gmail.com>
2024-10-14 03:11:55 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReservationSystem from './ReservationSystem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+      <ReservationSystem />
     </div>
   );
 }
